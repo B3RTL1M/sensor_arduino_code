3c8083303750833b080d9da9eeedf554a7edeea4
bertrand.limat <bertrand.limat@hefr.ch>
2017-06-18 11:57:00 +0200

    preparing files

---
+++ MPU6050/MPU6050.cpp
@@ -0,0 +1,17 @@
+/*
+MPU6050
+Created by B3RTL1M
+This code is completely open source, and you may edit it however you like. 
+Please support the Arduino community, and upload any bug fixes to the relative
+page. 
+Keep in mind that the code is for demonstration purposes only, I can't be
+held responsible if your device suddenly zaps you into millions of little
+atoms, and you live the rest of your existence in a quantum universe where
+nothing makes sense anymore (you know, you exist in more than one place, changing
+between matter and waves, while at the same time half your electrons are entangled
+in some other point in space and time). 
+Other than that, enjoy... =D
+*/
+
+#include "MPU6050.h"
+
---
+++ MPU6050/MPU6050.h
@@ -0,0 +1,27 @@
+/*
+MPU6050
+Created by B3RTL1M
+This code is completely open source, and you may edit it however you like. 
+Please support the Arduino community, and upload any bug fixes to the relative
+page. 
+Keep in mind that the code is for demonstration purposes only, I can't be
+held responsible if your device suddenly zaps you into millions of little
+atoms, and you live the rest of your existence in a quantum universe where
+nothing makes sense anymore (you know, you exist in more than one place, changing
+between matter and waves, while at the same time half your electrons are entangled
+in some other point in space and time). 
+Other than that, enjoy... =D
+*/
+
+#ifndef	MPU6050_h
+#define MPU6050_h
+
+
+class MPU6050 {
+	
+	public:
+	
+	private:
+	
+}
+#endif
